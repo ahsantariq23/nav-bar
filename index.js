c172615894111a9d71a0f1fb6b4587701a2cf329
Ahsan Tariq <ahsantariq250@gmail.com>
2022-09-08 00:32:19 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+document.querySelector("#menu").addEventListener("click" ,()=>{
+     
+document.querySelector("#heading").classList.toggle("remove");
+document.querySelector("ul").classList.toggle("ul-design");
+document.querySelector("nav").classList.toggle("nav-color");
+
+
+});
